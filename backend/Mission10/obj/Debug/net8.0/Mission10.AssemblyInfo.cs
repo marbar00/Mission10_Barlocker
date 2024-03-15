1da4979a147adefa877f60075f9129bb8e1bce19
Mark Jacob Barlocker <mjb364@byu.edu>
2024-03-14 18:17:51 -0600

    delete

--- backend/Mission10/obj/Debug/net8.0/Mission10.AssemblyInfo.cs
+++ backend/Mission10/obj/Debug/net8.0/Mission10.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31e674bbae4bdcee0ae45f1a24c3cff93fa98184")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
